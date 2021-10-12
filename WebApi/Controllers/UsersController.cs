3f7a447c0cb7ae33aa4bd75c4f6f60b11245e12c
= <=>
2021-10-12 17:19:12 +0500

    1 task

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.Security.Policy;
+using AutoMapper;
 using Game.Domain;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
@@ -9,21 +11,31 @@ namespace WebApi.Controllers
     [ApiController]
     public class UsersController : Controller
     {
+        IUserRepository userRepository;
+
+        IMapper mapper;
         // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
         }
 
+        [Produces("application/json", "application/xml")]
         [HttpGet("{userId}")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var user = userRepository.FindById(userId);
+            var res = mapper.Map<UserEntity, UserDto>(user);
+            
+            return Ok(res);
         }
 
         [HttpPost]
-        public IActionResult CreateUser([FromBody] object user)
+        public IActionResult CreateUser([FromBody] NewUserDto user)
         {
-            throw new NotImplementedException();
+
+            return Ok();
         }
     }
 }
---
+++ WebApi/Models/NewUserDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApi.Models
+{
+    public class NewUserDto
+    {
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }   
+    }
+}
